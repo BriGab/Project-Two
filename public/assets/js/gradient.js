84eda54e362d56ff756fa63d4976d42539fcd514
Cindy Burton <cindy.brton@gmail.com>
2020-02-24 19:47:47 -0700

    lots of little fixes and code cleanup

--- public/assets/js/gradient.js
+++ public/assets/js/gradient.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-use-before-define */
 
 $("#mood").on("click", function () {
     $("#select-mood").on("change", bgChoice(event));
@@ -12,66 +14,66 @@ function bgChoice(event) {
 
 function gradientSelection(value) {
     if (value == "Happy") {
-        granimInstance.changeState('cheerful-state');
+        granimInstance.changeState("cheerful-state");
     } else if (value == "Sad") {
-        granimInstance.changeState('sad-state');
+        granimInstance.changeState("sad-state");
     } else if (value == "Angry") {
-        granimInstance.changeState('angry-state');
+        granimInstance.changeState("angry-state");
     } else if (value == "Scared") {
-        granimInstance.changeState('scared-state');
+        granimInstance.changeState("scared-state");
     } else if (value == "Surprised") {
-        granimInstance.changeState('surprised-state');
+        granimInstance.changeState("surprised-state");
     } else {
-        granimInstance.changeState('cheerful-state');
+        granimInstance.changeState("cheerful-state");
     }
 }
 
 var granimInstance = new Granim({
-    element: '#canvas-basic',
-    direction: 'left-right',
+    element: "#canvas-basic",
+    direction: "left-right",
     isPausedWhenNotInView: true,
     states: {
         "default-state": {
             gradients: [
-                ['#B3FFAB', '#12FFF7'],
-                ['#ADD100', '#7B920A'],
-                ['#1A2980', '#26D0CE']
+                ["#B3FFAB", "#12FFF7"],
+                ["#ADD100", "#7B920A"],
+                ["#1A2980", "#26D0CE"]
             ],
             transitionSpeed: 10000
         },
         "sad-state": {
             gradients: [
-                ['#9D50BB', '#6E48AA'],
-                ['#D19592', '#8E54E9']
+                ["#9D50BB", "#6E48AA"],
+                ["#D19592", "#8E54E9"]
             ],
             transitionSpeed: 2000
         },
         "angry-state": {
             gradients: [
-                ['#FF4E50', '#F9D423'],
-                ['#cf2941', '#C81F70']],
+                ["#FF4E50", "#F9D423"],
+                ["#cf2941", "#C81F70"]],
             transitionSpeed: 10000
         },
         "surprised-state": {
             gradients: [
-                ['#B0F3F1', '#FFCFDF'],
-                ['#FF6B6B', '#556270'],
-                ['#C81F70', '#D19592']
+                ["#B0F3F1", "#FFCFDF"],
+                ["#FF6B6B", "#556270"],
+                ["#C81F70", "#D19592"]
             ],
             transitionSpeed: 10000
         },
         "scared-state": {
             gradients: [
-                ['#FF6B6B', '#556270'],
-                ['#80d3fe', '#7ea0c4'],
-                ['#f0ab51', '#eceba3']
+                ["#FF6B6B", "#556270"],
+                ["#80d3fe", "#7ea0c4"],
+                ["#f0ab51", "#eceba3"]
             ],
             transitionSpeed: 7000
         },
         "cheerful-state": {
             gradients: [
-                ['#FB7BA2', '#FCE043'],
-                ['#FFDD00', '#FBB034']
+                ["#FB7BA2", "#FCE043"],
+                ["#FFDD00", "#FBB034"]
             ],
             transitionSpeed: 10000
         }
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,33 +1,32 @@
 $(document).ready(function () {
-    $("#submit").on("click", function (event) {
-      event.preventDefault();
-      const email = $("#email").val().trim();
-      const name = $("#name").val().trim();
-      const userName = $("#username").val().trim();
-      const password = $("#password").val().trim();
-      signUp = {
-        email: email,
-        name: name,
-        username: userName,
-        password: password
-      };
-      if (!signUp.name.trim() || !signUp.username.trim() || !signUp.password.trim() || !signUp.email.trim()) {
-        M.toast({html: "Please fill in missing fields"})
-        return;
-      } else if ((signUp.password.length < 6) || (signUp.username.length < 6)) {
-        M.toast({html: "Username and password must be at least 6 characters"})
-      }
-      $.post("/api/users", signUp)
-        .then(function (data) {
-          console.log(signUp)
-          window.location.replace("/")
-        })
-        .catch(function (err) {
-          if (err) {
-            M.toast({html: "Username is already in use please try again"})
-          }
-        })
-  
-    })
-  })
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    const email = $("#email").val().trim();
+    const name = $("#name").val().trim();
+    const userName = $("#username").val().trim();
+    const password = $("#password").val().trim();
+    signUp = {
+      email: email,
+      name: name,
+      username: userName,
+      password: password
+    };
+    if (!signUp.name.trim() || !signUp.username.trim() || !signUp.password.trim() || !signUp.email.trim()) {
+      M.toast({ html: "Please fill in missing fields" });
+      return;
+    } else if ((signUp.password.length < 6) || (signUp.username.length < 6)) {
+      M.toast({ html: "Username and password must be at least 6 characters" });
+    }
+    $.post("/api/users", signUp)
+      .then(function () {
+        console.log(signUp);
+        window.location.replace("/");
+      })
+      .catch(function (err) {
+        if (err) {
+          M.toast({ html: "Username is already in use please try again" });
+        }
+      });
+  });
+});
 
